bc81231db79bd57082633f27a8604cfa33502fa7
xl <xuli2020sj@gmail.com>
2021-12-07 18:48:48 -0800

    basic

--- src/PrimaryGeneratorAction.cpp
+++ src/PrimaryGeneratorAction.cpp
@@ -8,12 +8,13 @@
 #include "G4ParticleDefinition.hh"
 #include "G4GeneralParticleSource.hh"
 #include "globals.hh"
+#include "G4SystemOfUnits.hh"
 
 PrimaryGeneratorAction::PrimaryGeneratorAction()
 	: G4VUserPrimaryGeneratorAction()
 {
-	theParticleSource = new G4GeneralParticleSource();
-	/*
+	//theParticleSource = new G4GeneralParticleSource();
+	
   G4int n_particle = 1;
   particleGun = new G4ParticleGun(n_particle);
 
@@ -25,24 +26,24 @@ PrimaryGeneratorAction::PrimaryGeneratorAction()
   particleGun->SetParticleDefinition(particle);
   particleGun->SetParticlePosition(G4ThreeVector(0.,0.,0.));
   particleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
-  particleGun->SetParticleEnergy(3.0*GeV);
-  */
+  particleGun->SetParticleEnergy(1.3325*MeV);
+  
 }
 
 PrimaryGeneratorAction::~PrimaryGeneratorAction()
 {
-	delete theParticleSource;
-	// delete particleGun;
+	//delete theParticleSource;
+	 delete particleGun;
 }
 
 // 每个event开始时被调用一次
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event *anEvent)
 {
-	/*
+	
   //G4double position = -0.5*(myDetector->GetWorldFullLength());
   particleGun->SetParticlePosition(G4ThreeVector(0.*cm,0.*cm,0.*cm));
 
   particleGun->GeneratePrimaryVertex(anEvent);
-  */
-	theParticleSource->GeneratePrimaryVertex(anEvent);
+  
+	//theParticleSource->GeneratePrimaryVertex(anEvent);
 }
--- src/PrimaryGeneratorAction.h
+++ src/PrimaryGeneratorAction.h
@@ -1,29 +1,31 @@
-#ifndef   PrimaryGeneratorAction_h
-#define   PrimaryGeneratorAction_h 1
+#ifndef PrimaryGeneratorAction_h
+#define PrimaryGeneratorAction_h 1
 
 #include "G4VUserPrimaryGeneratorAction.hh"
 #include "globals.hh"
-//class   DetectorConstruction;
-//class G4ParticleGun;
+#include "G4ParticleGun.hh"
+
+// class   DetectorConstruction;
+class G4ParticleGun;
 class G4Event;
 class G4GeneralParticleSource;
+class G4ParticleGun;
 
-class   PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
+class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
-  public:
-      PrimaryGeneratorAction( );
-      ~PrimaryGeneratorAction();
+public:
+	PrimaryGeneratorAction();
+	~PrimaryGeneratorAction();
 
-  public:
-    void GeneratePrimaries(G4Event*);
-    G4GeneralParticleSource* GetParticleSource() {return theParticleSource;};
+public:
+	void GeneratePrimaries(G4Event *);
+	//G4GeneralParticleSource *GetParticleSource() { return particleGun; };
+	const G4ParticleGun* GetParticleGun() const { return particleGun; }
 
-  private:
-    //G4ParticleGun* particleGun;
-    G4GeneralParticleSource* theParticleSource;
-    //DetectorConstruction* myDetector;
+private:
+	G4ParticleGun* particleGun;
+	// G4GeneralParticleSource* theParticleSource;
+	// DetectorConstruction* myDetector;
 };
 
 #endif
-
-
